93051184788b3db200c4076fb5504639ee28c76f
Dan Masquelier <dancrew32@gmail.com>
2011-06-23 22:25:23 -0700

    running through the dive into python tutorials

---
+++ odbchelper.py
@@ -0,0 +1,15 @@
+def buildConnectionString(params):
+    """Build a connection string from a dictionary of parameters
+
+    Returns string."""
+    return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
+
+if __name__ == "__main__":
+    myParams = {"server":"localhost", \
+                "database":"diveintopython", \
+                "uid":"root", \
+                "pwd":"root" \
+               }
+    print buildConnectionString(myParams)
+
+
