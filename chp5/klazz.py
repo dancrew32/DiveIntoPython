4424137e0d355c2908e31f451509f485674d10cc
Dan Masquelier <dancrew32@gmail.com>
2011-06-28 23:23:39 -0700

    typical class/methods

---
+++ chp5/klazz.py
@@ -0,0 +1,9 @@
+class Loaf:
+    pass # move along, nothing to see here method, like {} in C/Java
+
+
+class BeefCake:
+    'i describe the class that is BeefCake'
+    def __init__(self): # basically the constructor, called immediately, 
+        'i describe the method'
+# self is always the first argument for methods in a class
